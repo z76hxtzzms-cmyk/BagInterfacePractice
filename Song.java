9f44189138d9eaab688891975923ad4d621df4b6
jstn131 <jh013107@icloud.com>
2026-02-18 23:28:31 -0800

    inital commit

---
+++ Song.java
@@ -0,0 +1,17 @@
+public class Song {
+    private String title;
+    private String artist;
+
+    public Song(String title, String artist) {
+        this.title = title;
+        this.artist = artist;
+    }
+
+    public String getTitle() { 
+        return title; 
+    }
+
+    public String getArtist() { 
+        return artist; 
+    }
+}
